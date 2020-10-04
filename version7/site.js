851587f1ad2fb8545d5d2ca7cc560fa1ebabe881
Kond <peter.tamas@greenfoxacademy.com>
2020-10-04 21:53:34 +0300

    feat(version 7): about us

--- version7/site.js
+++ version7/site.js
@@ -1,3 +1,28 @@
+function initEventHanlders() {
+  const modalOverlay = document.getElementById("modal-overlay");
+  const modals = [...document.getElementsByClassName("modal")];
+  const laraModal = document.getElementById("lara-modal");
+  const nadiaModal = document.getElementById("nadia-modal");
+  const closeButtons = [...document.getElementsByClassName("close")];
+
+  document.getElementById("show-more-lara").addEventListener("click", () => {
+    modalOverlay.classList.toggle("closed");
+    laraModal.classList.toggle("closed");
+  });
+
+  document.getElementById("show-more-nadia").addEventListener("click", () => {
+    modalOverlay.classList.toggle("closed");
+    nadiaModal.classList.toggle("closed");
+  });
+
+  [modalOverlay, ...closeButtons].forEach((c) =>
+    c.addEventListener("click", () => {
+      modalOverlay.classList.toggle("closed");
+      modals.forEach((m) => m.classList.add("closed"));
+    })
+  );
+}
+
 function flyingBoyHandler() {
   appearanceHandler();
   // scrollHandler();
@@ -6,21 +31,24 @@ function flyingBoyHandler() {
 function scrollHandler() {
   // const startY = document.getElementById('idea').getBoundingClientRect().y;
   const startY = 100;
-  const boy = document.getElementById('hero-img');
+  const boy = document.getElementById("hero-img");
   const originalBoyHeight = boy.getBoundingClientRect().height;
-  window.addEventListener('scroll', (e) => {
+  window.addEventListener("scroll", (e) => {
     let y = window.scrollY;
     let screenHeight = window.innerHeight;
     let distance = y;
     if (y >= startY) {
-      boy.animate({
-        height: '100px',
-        transform
-      }, 200);
-      setTimeout(() => boy.style.height = '100px', 200)
+      boy.animate(
+        {
+          height: "100px",
+          transform,
+        },
+        200
+      );
+      setTimeout(() => (boy.style.height = "100px"), 200);
     } else {
-      boy.animate({height: `${originalBoyHeight}px`}, 200);
-      setTimeout(() => boy.style.height = `${originalBoyHeight}px`, 200)
+      boy.animate({ height: `${originalBoyHeight}px` }, 200);
+      setTimeout(() => (boy.style.height = `${originalBoyHeight}px`), 200);
     }
   });
 }
@@ -31,16 +59,16 @@ function appearanceHandler() {
     "IntersectionObserverEntry" in window &&
     "intersectionRatio" in window.IntersectionObserverEntry.prototype
   ) {
-    let observer = new IntersectionObserver(entries => {
+    let observer = new IntersectionObserver((entries) => {
       if (entries[0].boundingClientRect.y < 0) {
         document.getElementById("flying-boy").classList.remove("hidden");
       } else {
         document.getElementById("flying-boy").classList.add("hidden");
       }
     });
-  
+
     observer.observe(document.querySelector("#flying-boy-appearance-trigger"));
   }
 }
 
-document.onload = flyingBoyHandler();
+document.onload = initEventHanlders();
